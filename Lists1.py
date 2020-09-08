8f01e28e15109f13e2d0f531e190f9276835e8e0
Sushma Nandiyawar <sushmanandiyawar5621@gmail.com>
2020-09-08 15:35:13 +0530

    third commit

--- Lists1.py
+++ Lists1.py
@@ -7,13 +7,13 @@
 
 fname = raw_input("Enter file name: ")
 fh = open(fname)
-lst = list()                       # list for the desired output
-for line in fh:                    # to read every line of file romeo.txt
-    word= line.rstrip().split()    # to eliminate the unwanted blanks and turn the line into a list of words
-    for element in word:           # check every element in word    
-        if element in lst:         # if element is repeated
-            continue               # do nothing
+lst = list()                       
+for line in fh:                    
+    word= line.rstrip().split()    
+    for element in word:              
+        if element in lst:         
+            continue               
         else :                     
-			lst.append(element)    # append     
-lst.sort()                         # sort the list
+			lst.append(element)     
+lst.sort()                         
 print(lst)
